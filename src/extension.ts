f7bceaec596038c651ec66db1db444585b287d76
RoBaaaT <robert.lasch0@googlemail.com>
2021-09-28 12:20:58 +0200

    Automatically insert a reference to created meeting log files in the daily log file

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import path = require('path');
 import * as vscode from 'vscode';
 
 function getCurrentDateString(): string {
     let date = new Date();
     return date.getFullYear().toString().padStart(4, '0') + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+}
+
+function getDailyUri(dateString: string): vscode.Uri {
+    return vscode.Uri.file(vscode.workspace.workspaceFolders![0].uri.fsPath + '/daily/' + dateString + '.md');
 }
 
 // this method is called when your extension is activated
@@ -15,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('markdown-log-utils.createDaily', async () => {
         let edit = new vscode.WorkspaceEdit();
         let dateString = getCurrentDateString();
-        let uri = vscode.Uri.file(vscode.workspace.workspaceFolders![0].uri.fsPath + '/daily/' + dateString + '.md');
+        let uri = getDailyUri(dateString);
         edit.createFile(uri, { ignoreIfExists: true });
         let created = await vscode.workspace.applyEdit(edit);
         if (created) {
@@ -40,6 +45,7 @@ export function activate(context: vscode.ExtensionContext) {
             edit.createFile(uri, { ignoreIfExists: true });
             let created = await vscode.workspace.applyEdit(edit);
             if (created) {
+                // create header for the meeting file
                 let doc = await vscode.workspace.openTextDocument(uri);
                 let editor = await vscode.window.showTextDocument(doc);
                 if(doc.getText() === '') {
@@ -47,6 +53,15 @@ export function activate(context: vscode.ExtensionContext) {
                         editBuilder.insert(doc.positionAt(0), '# ' + meetingName + ' (' + dateString + ')\n');
                     });
                 }
+
+                // insert link to the meeting in the daily log file
+                let dailyUri = getDailyUri(dateString);
+                let dailyDoc = await vscode.workspace.openTextDocument(dailyUri);
+                let dailyEditor = await vscode.window.showTextDocument(dailyDoc, undefined, true);
+                let refToMeeting = '[' + meetingName + '](' + path.relative(path.dirname(dailyUri.fsPath), uri.fsPath) + ')\n';
+                dailyEditor.edit(editBuilder => {
+                    editBuilder.replace(dailyEditor.selection, refToMeeting);
+                });
             }
         }
 	}));
